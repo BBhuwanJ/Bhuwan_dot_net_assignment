6ce7afeba7c7c0a51802694f0dfb39901498ceff
BBhuwanJ <mrstrange564@gmail.com>
2025-01-31 20:22:14 +0545

    Add project files.

---
+++ OnlineVotingSystem/Models/DbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OnlineVotingSystem.Areas.Identity.Data;
+using OnlineVotingSystem.Data;
+
+namespace OnlineVotingSystem.Models
+{
+    public static class DbInitializer
+    {
+        public static async Task InitializeAsync(OnlineVotingSystemContext context, IServiceProvider serviceProvider, UserManager<OnlineVotingSystemUser> userManager)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string[] roleNames = { "Admin", "Voter", "Candidate" };
+            IdentityResult roleResult;
+            foreach (var RoleName in roleNames)
+            {
+                var roleExists = await RoleManager.RoleExistsAsync(RoleName);
+                if (!roleExists)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(RoleName));
+
+                }
+            }
+            string Email = "admin@eemc.com";
+            string Password = "Admin@123";
+            if (userManager.FindByEmailAsync(Email).Result == null)
+            {
+                OnlineVotingSystemUser user = new OnlineVotingSystemUser();
+                user.UserName = Email;
+                user.Email = Email;
+                IdentityResult result = userManager.CreateAsync(user, Password).Result;
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+
+
+        }
+    }
+}
---
+++ OnlineVotingSystem/ReadMe.txt
@@ -0,0 +1,14 @@
+﻿1 - Add Identity matrics thorught Scaffolded item 
+2 - Add properties inside OnlineVotingSystemUser.cs like name, address, isVoter etc
+3 - Create a DbInitializer Class in Models, and Set the identity of of Admin, Candidate, Voter and set the admin UserName and Password
+4 - Make small changes in main Program.cs
+5 - add-migration Init
+6 - update-database
+7 - Add all the properties into Register.cshtml.cs
+8 - Created Register page and registration form in Register.cshtml
+9 - Inlcude _LoginPartial components in _Layout.cshtml
+10 - Then, In register.cshtml.cs we take inputs from user as in email, username, password, address and profile picture, and since we
+	can't just add profile picture,we upload that picture in wwwroot>Images and give the string Imagepath to the user.ProfileImage.
+	Remember: System ma jaane chai string nai ho, but upload garda kheri matra IFormFile format use garne ho since it is file.
+	entype ma pani multipart/formdata user garni
+11 - 
